7c46d8452b1bd7a2306b14e120faa7eaf0bc7d55
pholdahl <pal.holdahl@gmail.com>
2022-10-06 21:53:24 +0200

    Pål har endret kode på: - DobbeltLenkeListe(T[] a) - boolean leggInn(T verdi) - toString() - omvendtString()
    Oppgave 1 og 2 burde være løst.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -6,6 +6,7 @@ package no.oslomet.cs.algdat.Oblig2;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Objects;
 
 
 public class DobbeltLenketListe<T> implements Liste<T> {
@@ -41,31 +42,27 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
     public DobbeltLenketListe(T[] a) {
-        // TODO: Pål gjør noen endringer her straks!
-        this();   // nuller alle variabelene
-
-        if (a.length==0){
-            // kastes en NullPointerException med teksten "Tabellen a er
-            //null!" (bruk f.eks. en requireNonNull-metode fra klassen Objects).
-        }
-        int i=0;
-        for (; i< a.length && a[i]==null; i++);
-        if (i<a.length){
-            Node<T> n= hode= new Node<>(a[i]);
-            hode.forrige=null;
-            antall=1;
-
-            for (i++;i<a.length;i++){
-                if (a[i]!=null){
-                    n.neste=new Node<>(a[i]);
-                    n.neste.forrige=n;
-                    n=n.neste;
-
-                    antall++;
+        Objects.requireNonNull(a, "Tabellen a er null!");               // godtar ikke a = null; a = {} er greit.
+        if (a.length > 0) {                                                     // så lenge a.length er større enn 0
+            int i = 0;                                                          // initieres i
+            for (; i < a.length && a[i] == null; i++);                          // teller opp i fra 0 til første verdi som ikke er null
+            if (i < a.length) {                                                 // så lenge i er mindre enn lengden til tabellen
+                Node<T> p = hode = new Node<>(a[i], null, null);    // setter første verdi som ikke er null til hode som første node
+                antall++;                                                       // øker antall
+                i++;                                                            // øker i, da første verdi som ikke er null er funnet
+                for (; i < a.length; i++) {                                     // for løkke på resten av a
+                    if (a[i] != null) {                                         // så lenge a[i] ikke er en null verdi
+                        p.neste = new Node<>(a[i]);                             // lager en node på p sin neste
+                        p.neste.forrige = p;                                    // setter peker til neste p til forrige
+                        p = p.neste;                                            // gjør p lik p sin neste
+                        antall++;                                               // øker antall
+                    }
                 }
+                hale = p;                                                       // p er siste node, gjør hale lik p
+                hale.forrige = p.forrige;                                       // gjør hale sin forrige lik p sin forrige
+                hale.neste = null;                                              // gjør hale sin neste lik null
+                endringer++;                                                    // å legge inn et array = 1 endring
             }
-            hale= n;
-            hale.neste=null;
         }
     }
 
@@ -75,13 +72,11 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public int antall() {
-
         return antall;
     }
 
     @Override
     public boolean tom() {
-
         if (antall==0) {
             return true;
         }
@@ -90,8 +85,20 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public boolean leggInn(T verdi) {
-        // TODO: Det skal legges inn helt på slutten av listen, boolsk return!
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(verdi, "Ikke tillatt med null-verider!");    // godtar ikke null-verdier
+        if(antall == 0){                                                            // hvis antallet er 0, så er det ingen verdier i listen
+            hode = hale = new Node<>(verdi, null, null);                // da blir hade lik hode lik den aller første noden
+            antall++;                                                               // øker antall
+            endringer++;                                                            // øker endringer
+            return true;                                                            // returnerer true
+        }
+        hale.neste = new Node<>(verdi);                                             // hvis antallet er større enn 0 lager vi neste node etter hale
+        hale.neste.forrige = hale;                                                  // setter forrige-pekeren til siste node til noden hale peker på
+        hale = hale.neste;                                                          // setter hale til å peke på siste node
+        antall++;                                                                   // øker antall
+        endringer++;                                                                // øker endringer
+        return true;
+
     }
 
     @Override
@@ -136,13 +143,37 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public String toString() {
-        // TODO: Her kommer det en toString metode som bruker stringBuilder()
-        throw new UnsupportedOperationException();
+        StringBuilder s = new StringBuilder();      // bruker stringBuilder
+        s.append("[");                              // starter med  [
+        if(antall == 0) {                           // så lenge antallet er 0
+            s.append("]");                          // slenger vi bare på en til ]
+            return s.toString();                    // returnerer
+        }
+        Node<T> p = hode;                           // hvis antallet er mer enn 0 lager vi en p variabel til å traversere fra hode til halen
+        s.append(p.verdi);                          // legger til den første verdien
+        for(int i = 1; i < antall; i++) {           // bruker en for-løkke for å traversere og hente ut resten
+            p = p.neste;                            // går videre til neste
+            s.append(", ").append(p.verdi);         // legger til , og neste verdi
+        }
+        s.append("]");                              // etter for løkken legges ] til
+        return s.toString();                        // returnerer
     }
 
     public String omvendtString() {
-        // TODO: Denne toString() metoden skal gå motsatt vei. HUSK! antall-1 i for løkken!
-        throw new UnsupportedOperationException();
+        StringBuilder s = new StringBuilder();      // bruker stringBuilder
+        s.append("[");                              // starter med  [
+        if(antall == 0) {                           // så lenge antallet er 0
+            s.append("]");                          // slenger vi bare på en til ]
+            return s.toString();                    // returnerer
+        }
+        Node<T> p = hale;                           // hvis antallet er mer enn 0 lager vi en p variabel til å traversere fra halen til hode
+        s.append(p.verdi);                          // legger til verdien i halen
+        for(int i = antall-1; i > 0; i--) {         // begynner på antall-1, siden antall er det sammen som length
+            p = p.forrige;                          // går videre til den forrige
+            s.append(", ").append(p.verdi);         // legger til , og forrige verdi
+        }
+        s.append("]");                              // etter for løkken legges ] til
+        return s.toString();                        // returnerer
     }
 
     @Override
