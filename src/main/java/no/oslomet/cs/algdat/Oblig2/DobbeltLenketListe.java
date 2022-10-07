17a9f435257aa873fb489865157028321debf298
LKielland <leifkielland@runbox.com>
2022-10-07 11:02:21 +0200

    main()

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -9,6 +9,14 @@ import java.util.*;
 
 public class DobbeltLenketListe<T> implements Liste<T> {
 
+    public static void main(String[] args) {
+        Character[] c = {'A','B','C','D','E','F','G','H','I','J',};
+        DobbeltLenketListe<Character> liste = new DobbeltLenketListe<>(c);
+        System.out.println(liste.subliste(3,8));  // [D, E, F, G, H]
+        System.out.println(liste.subliste(5,5));  // []
+        System.out.println(liste.subliste(8,liste.antall()));  // [I, J]
+        // System.out.println(liste.subliste(0,11));  // skal kaste unntak
+    }
     /**
      * Node class
      *
